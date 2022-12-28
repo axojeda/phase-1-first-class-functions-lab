c2b1fa57a430452ea70c01d00bdb64ace7fe55a9
Aura <ax.pink@gmail.com>
2022-12-27 19:31:48 -0500

    commit tree 94bc78895e18238d9c39e88925fabd28a96ad145

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.splice(0, 2);
+
+    }
+    const returnLastTwoDrivers = (drivers) => {
+        return drivers.slice(2, 3)
+  };
+  
+  console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+  console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']))
+
+  
+ 
